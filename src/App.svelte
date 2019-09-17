df25bd3d306344711c0aab109522b0596a906444
Shaye Ulman <shayeulman@gmail.com>
2019-09-17 15:40:22 +0300

    some little changes

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,14 @@
 	import Rabbit from './app';
 
 	const rabbit = new Rabbit('male');
-	console.log(rabbit.timeOfBirth)
 	const func = () => {
-		console.log(rabbit.position())
 		console.log(rabbit.age())
 	}
 </script>
 
 <p></p>
 <button on:click={ func }>test</button>
+<p>{ rabbit.timeOfBirth }</p>
+<p>{ rabbit.position().x }</p>
+
 
